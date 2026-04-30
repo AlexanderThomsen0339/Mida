32f0908d274dd70786ef5f94cdca9d535b56f81d
Alexander Thomsen <n1majt@aalborg.dk>
2026-04-30 12:58:28 +0200

    fix

--- API/main.py
+++ API/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from API.routers import sources, jobs, trigger, auth, data
+
+from routers import sources, jobs, trigger, auth, data
 
 app = FastAPI(title="MIDA API", version="1.0.0")
 
-app.include_router(auth.router,    prefix="/auth",    tags=["Auth"])
+app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(sources.router, prefix="/sources", tags=["Sources"])
-app.include_router(jobs.router,    prefix="/jobs",    tags=["Jobs"])
+app.include_router(jobs.router, prefix="/jobs", tags=["Jobs"])
 app.include_router(trigger.router, prefix="/trigger", tags=["Trigger"])
 app.include_router(data.router, prefix="/data", tags=["Data"])
-
 
 app.add_middleware(
     CORSMiddleware,
